7bffa8d36a71ad543f8833298e03cf8cf47e359d
yellpall <ohj3423@naver.com>
2024-01-04 23:34:13 +0900

    feat: Add PlainTextRepository.java

--- src/main/java/com/samdaseuss/api/introductiontospringbatch/core/domain/PlainText.java
+++ src/main/java/com/samdaseuss/api/introductiontospringbatch/core/domain/PlainText.java
@@ -1,2 +1,21 @@
-package com.samdaseuss.api.introductiontospringbatch.core.domain;public class PlainText {
+package com.samdaseuss.api.introductiontospringbatch.core.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "plain_text")
+public class PlainText {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String text;
+
 }

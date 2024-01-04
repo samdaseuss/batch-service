041694d2123242e6bf44eae19b7447d0bd2ca5a9
yellpall <ohj3423@naver.com>
2024-01-04 10:01:55 +0900

    feat: Add PlainTextRepository.java
    - 페이징 통해 데이터 읽어들이기 위해 쿼리 추가 : Page<PlainText> findBy(Pageable pageable);
    - result : 페이지 사이즈 만큼 데이터를 읽어들이게 된다.

---
+++ src/main/java/com/samdaseuss/api/introductiontospringbatch/core/domain/PlainText.java
@@ -0,0 +1,2 @@
+package com.samdaseuss.api.introductiontospringbatch.core.domain;public class PlainText {
+}
---
+++ src/main/java/com/samdaseuss/api/introductiontospringbatch/core/repository/PlainTextRepository.java
@@ -0,0 +1,10 @@
+package com.samdaseuss.api.introductiontospringbatch.core.repository;
+
+import com.samdaseuss.api.introductiontospringbatch.core.domain.PlainText;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlainTextRepository extends JpaRepository<PlainText, Integer> {
+    Page<PlainText> findBy(Pageable pageable);
+}

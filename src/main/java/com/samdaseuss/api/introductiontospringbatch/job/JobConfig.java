42d0b96a7995c57a83fc74eda33e032859974877
samdaseuss <ohj3423@naver.com>
2024-01-05 00:30:02 +0900

    feat: Update JobConfig.java
    - 청크 기반 처리를 통한 배치 구현
    - 리더 작성해 데이터를 읽어온다.

--- src/main/java/com/samdaseuss/api/introductiontospringbatch/job/JobConfig.java
+++ src/main/java/com/samdaseuss/api/introductiontospringbatch/job/JobConfig.java
@@ -1,5 +1,7 @@
 package com.samdaseuss.api.introductiontospringbatch.job;
 
+import com.samdaseuss.api.introductiontospringbatch.core.domain.PlainText;
+import com.samdaseuss.api.introductiontospringbatch.core.repository.PlainTextRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
@@ -9,9 +11,16 @@ import org.springframework.batch.core.configuration.annotation.StepBuilderFactor
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.data.RepositoryItemReader;
+import org.springframework.batch.item.data.builder.RepositoryItemReaderBuilder;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.Sort;
+
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
@@ -19,31 +28,37 @@ public class JobConfig {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
+    private final PlainTextRepository plainTextRepository;
 
     @Bean("batchJob")
     public Job batchJob(Step batchStep) {
         return jobBuilderFactory
                 .get("batchJob")
-                .incrementer(new RunIdIncrementer()) // 잡을 실행할 때 횟수를
+                .incrementer(new RunIdIncrementer())
                 .start(batchStep)
                 .build();
     }
 
     @JobScope
     @Bean("batchStep")
-    public Step batchStep(Tasklet tasklet) {
+    public Step batchStep(ItemReader batchReader) {
         return stepBuilderFactory
                 .get("batchStep")
-                .tasklet(tasklet)
+                .<PlainText, String>chunk(5)
+                .reader(batchReader)
                 .build();
     }
 
     @StepScope
     @Bean
-    public Tasklet tasklet() {
-        return (contribution, chunkContext) -> {
-            System.out.println("batch test");
-            return RepeatStatus.FINISHED;
-        };
+    public RepositoryItemReader<PlainText> batchReader() {
+        return new RepositoryItemReaderBuilder<PlainText>()
+                .name("plainTextReader")
+                .repository(plainTextRepository)
+                .methodName("findBy")
+                .pageSize(5)
+                .arguments(List.of())
+                .sorts(Collections.singletonMap("id", Sort.Direction.DESC))
+                .build();
     }
 }

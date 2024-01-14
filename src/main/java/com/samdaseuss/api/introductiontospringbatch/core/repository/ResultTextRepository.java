a4d6d547ff413a47f8f1568a855f34e1293b0720
samdaseuss <ohj3423@naver.com>
2024-01-14 13:30:56 +0900

    feat: Add core/repository/ResultTextRepository.java

--- src/main/java/com/samdaseuss/api/introductiontospringbatch/core/repository/ResultTextRepository.java
+++ src/main/java/com/samdaseuss/api/introductiontospringbatch/core/repository/ResultTextRepository.java
@@ -4,4 +4,4 @@ import com.samdaseuss.api.introductiontospringbatch.core.domain.ResultText;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ResultTextRepository extends JpaRepository<ResultText, Integer> {
-}s
+}

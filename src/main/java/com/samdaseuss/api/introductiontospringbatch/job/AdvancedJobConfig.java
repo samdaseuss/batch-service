950f9101b146be57160162f2184e55701d045f0b
samdaseuss <ohj3423@naver.com>
2024-01-14 14:01:05 +0900

    feat: Add job/AdvancedJobConfig.java

---
+++ src/main/java/com/samdaseuss/api/introductiontospringbatch/job/AdvancedJobConfig.java
@@ -0,0 +1,50 @@
+package com.samdaseuss.api.introductiontospringbatch.job;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@AllArgsConstructor
+@Slf4j
+public class AdvancedJobConfig {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job advancedJob(Step advancedStep) {
+        return jobBuilderFactory.get("advancedJob")
+                .incrementer(new RunIdIncrementer())
+                .start(advancedStep)
+                .build();
+    }
+
+    @JobScope
+    @Bean
+    public Step advancedStep(Tasklet advancedTasklet) {
+        return stepBuilderFactory.get("advancedStep")
+                .tasklet(advancedTasklet)
+                .build();
+    }
+
+    @StepScope
+    @Bean
+    public Tasklet advancedTasklet() {
+        return (contribution, chunkContext) -> {
+            log.info("[AdvancedJobConfig] excuted advancedTasklet");
+            return RepeatStatus.FINISHED;
+        };
+    }
+
+}
